0a5c57bc36b99b667560d2af052dac10debf524e
Jerge <giannakakos97@gmail.com>
2023-07-08 18:11:13 +0200

    Recode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,151 +1,97 @@
-        var noten = [];
+const noten = [];
+const gewichtungen = [];
 
-        var gewichtungen = [];
+const noteInput = document.getElementById("note");
+const gewichtungSelect = document.getElementById("gewichtung");
+let initialNoteValue = noteInput.value;
+let initialGewichtungValue = gewichtungSelect.value;
 
-  
+function berechneDurchschnitt() {
+  const note = parseFloat(noteInput.value);
+  const gewichtung = gewichtungSelect.value;
 
-        var noteInput = document.getElementById("note");
-        var gewichtungSelect = document.getElementById("gewichtung");
-        var initialNoteValue = noteInput.value;
-        var initialGewichtungValue = gewichtungSelect.value;
-        
-        function berechneDurchschnitt() {
-          var note = parseFloat(noteInput.value);
-          var gewichtung = gewichtungSelect.value;
-        
-          if (note >= 1 && note <= 6) {
-            noten.push(note);
-            gewichtungen.push(gewichtung);
-            updateNotenListe();
-            berechneUndAktualisiereDurchschnitt();
-          }
-        
-          noteInput.value = initialNoteValue;
-          gewichtungSelect.value = initialGewichtungValue;
-        
-          noteInput.focus();
-        }
-        
-        noteInput.addEventListener("input", function() {
-          initialNoteValue = noteInput.value;
-        });
-        
-        gewichtungSelect.addEventListener("change", function() {
-          initialGewichtungValue = gewichtungSelect.value;
-        });
-        
-        
-        
+  if (note >= 1 && note <= 6) {
+    noten.push(note);
+    gewichtungen.push(gewichtung);
+    updateNotenListe();
+    berechneUndAktualisiereDurchschnitt();
+  }
 
-  
+  noteInput.value = initialNoteValue;
+  gewichtungSelect.value = initialGewichtungValue;
+}
 
-        function updateNotenListe() {
+noteInput.addEventListener("input", function() {
+  initialNoteValue = noteInput.value;
+});
 
-          var notenListe = document.getElementById("notenListe");
+gewichtungSelect.addEventListener("change", function() {
+  initialGewichtungValue = gewichtungSelect.value;
+});
 
-          notenListe.innerHTML = "";
+function updateNotenListe() {
+  const notenListe = document.getElementById("notenListe");
+  notenListe.innerHTML = "";
 
-  
+  for (let i = 0; i < noten.length; i++) {
+    const noteEintrag = document.createElement("li");
+    const gewichtungText = {
+      "A": "2",
+      "B": "1",
+      "C": "0.5",
+    }[gewichtungen[i]];
 
-          for (var i = 0; i < noten.length; i++) {
+    noteEintrag.textContent = "Note: " + noten[i] + ", Gewichtung: " + gewichtungText;
 
-            var noteEintrag = document.createElement("li");
+    const entfernenButton = document.createElement("button");
+    entfernenButton.textContent = "X";
+    entfernenButton.className = "entfernenButton";
+    entfernenButton.setAttribute("data-index", i);
+    entfernenButton.onclick = entferneNote;
 
-            var gewichtungText = "";
+    noteEintrag.appendChild(entfernenButton);
+    notenListe.appendChild(noteEintrag);
+  }
+}
 
-  
+function entferneNote(event) {
+  const index = event.target.getAttribute("data-index");
+  noten.splice(index, 1);
+  gewichtungen.splice(index, 1);
+  updateNotenListe();
+  berechneUndAktualisiereDurchschnitt();
+}
 
-            switch (gewichtungen[i]) {
+function berechneUndAktualisiereDurchschnitt() {
+  const gewicht = { "A": 2, "B": 1, "C": 0.5 };
+  const gewichteteNoten = noten.map((note, i) => note * gewicht[gewichtungen[i]]);
+  const summe = gewichteteNoten.reduce((a, b) => a + b, 0);
+  const gewichtungSumme = gewichtungen.map(g => gewicht[g]).reduce((a, b) => a + b, 0);
 
-              case "A":
+  const durchschnitt = gewichtungSumme !== 0 ? summe / gewichtungSumme : 0;
+  const durchschnittElement = document.getElementById("durchschnitt");
+  durchschnittElement.textContent = "Der gewichtete Durchschnitt der Noten ist: " + durchschnitt.toFixed(3);
+}
 
-                gewichtungText = "2";
+function alleNotenLoeschen() {
+  noten.length = 0;
+  gewichtungen.length = 0;
+  updateNotenListe();
+  berechneUndAktualisiereDurchschnitt();
+}
 
-                break;
+document.getElementById("notenForm").addEventListener("submit", berechneDurchschnitt);
+document.getElementById("alleLoeschenButton").addEventListener("click", alleNotenLoeschen);
 
-              case "B":
-
-                gewichtungText = "1";
-
-                break;
-
-              case "C":
-
-                gewichtungText = "0.5";
-
-                break;
-
-            }
-
-  
-
-            noteEintrag.textContent = "Note: " + noten[i] + ", Gewichtung: " + gewichtungText;
-
-  
-
-            var entfernenButton = document.createElement("button");
-
-            entfernenButton.textContent = "X";
-
-            entfernenButton.className = "entfernenButton"; // Hinzufügen der Klasse
-
-            entfernenButton.setAttribute("data-index", i);
-
-            entfernenButton.onclick = entferneNote;
-
-  
-
-            noteEintrag.appendChild(entfernenButton);
-
-            notenListe.appendChild(noteEintrag);
-
-          }
-
-        }
-
-  
-
-        function entferneNote(event) {
-
-          var index = event.target.getAttribute("data-index");
-
-          noten.splice(index, 1);
-
-          gewichtungen.splice(index, 1);
-
-          updateNotenListe();
-
-          berechneUndAktualisiereDurchschnitt();
-
-        }
-
-  
-
-        function berechneUndAktualisiereDurchschnitt() {
-            var summe = 0;
-            var gewichtungSumme = 0;
-          
-            for (var i = 0; i < noten.length; i++) {
-              var note = noten[i];
-              var gewichtung = gewichtungen[i];
-          
-              var gewicht = { "A": 2, "B": 1, "C": 0.5 };
-              var gewichtungWert = gewicht[gewichtung];
-          
-              summe += note * gewichtungWert;
-              gewichtungSumme += gewichtungWert;
-            }
-          
-            var durchschnitt = gewichtungSumme !== 0 ? summe / gewichtungSumme : 0;
-            var durchschnittElement = document.getElementById("durchschnitt");
-            durchschnittElement.textContent = "Der gewichtete Durchschnitt der Noten ist: " + durchschnitt.toFixed(3);
-          }
-          
-
-        function alleNotenLoeschen() {
-            noten = [];
-            gewichtungen = [];
-            updateNotenListe();
-            berechneUndAktualisiereDurchschnitt();
-          }
-          
+document.getElementById("shareButton").addEventListener("click", function () {
+  if (navigator.share) {
+    navigator.share({
+      title: "Notenrechner Online",
+      url: window.location.href,
+    })
+      .then(() => console.log("Erfolgreich geteilt"))
+      .catch((error) => console.log("Fehler beim Teilen:", error));
+  } else {
+    alert("Die Teilen-Funktion wird von Ihrem Browser nicht unterstützt.");
+  }
+});
